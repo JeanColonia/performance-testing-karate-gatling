7a45a2c5ca19dd3ebbf6f7e5b4284a3e0562321a
jcolonia <jcolonia@baufest.com>
2024-07-02 16:28:12 -0500

    injection scripts

--- src/test/java/performance/PerformanceTest.scala
+++ src/test/java/performance/PerformanceTest.scala
@@ -16,7 +16,15 @@ class PerformanceTest extends Simulation {
   val create = scenario("get pets by pending status").exec(karateFeature("classpath:performance/getPets.feature"))
 
   setUp(
-    create.inject(atOnceUsers(3)).protocols(protocol)
+    create.inject(
+      atOnceUsers(3), //3
+      nothingFor(4 seconds),
+      constantUsersPerSec(1).during(10 seconds), //10
+      constantUsersPerSec(2).during(10 seconds), //20 
+      rampUsersPerSec(2).to(10).during(10 seconds), //2-10
+      nothingFor(4 seconds)
+
+      ).protocols(protocol)
   )
 
 }
